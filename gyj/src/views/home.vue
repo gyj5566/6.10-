ddea38bb3981aa869240fa3a439d325522c09205
gyj5566 <2942353454@qq.com>
2019-06-10 10:37:17 +0800

    init
    init

---
+++ gyj/src/views/home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <my-header></my-header>
+        home
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
